3f03115e6e2ff317fb8b8d68ac895118c2f68268
Dkodsy <dkods15@mail.ru>
2021-01-15 04:13:14 +0600

    Добавил возможность отправлять в рассылке: видео, фото, файлы

--- handlers/users/admins.py
+++ handlers/users/admins.py
@@ -14,12 +14,6 @@ async def send_to_admin(message: types.Message):
     await message.answer(text='Ку', reply_markup=keyboard_for_admin)
 
 
-@dp.message_handler(IsAdmin(), text='Рассылка поста для юзеров')
-async def enter_text(message: types.Message):
-    await message.answer(text='Введите текст поста')
-    await Admins.enter_text.set()
-
-
 @dp.message_handler(IsAdmin(), text='Сколько сейчас пользователей?')
 async def enter_text(message: types.Message):
     count_users = await db.count_users()
@@ -33,27 +27,73 @@ async def enter_text(message: types.Message):
     await message.answer(f"Список пользователей\n{''.join(format_all_users)}")
 
 
-@dp.message_handler(state=Admins.enter_text)
+@dp.message_handler(IsAdmin(), text='Рассылка поста для юзеров')
+async def enter_text(message: types.Message):
+    await message.answer(text='Введите текст поста')
+    await Admins.enter_text.set()
+
+
+@dp.message_handler(state=Admins.enter_text, content_types=types.ContentTypes.VIDEO)
+async def text_check(message: types.Message, state: FSMContext):
+    video_id = message.video.file_id
+    caption = message.caption
+    await state.update_data(video=video_id)
+    await state.update_data(caption=caption)
+    await message.answer(f'Проверьте введенное сообщение(оступы и форматирование сохранено)\n\n',
+                         reply_markup=cancel_or_accept)
+    await message.answer_video(video_id, caption=caption)
+    await Admins.check_text.set()
+
+
+@dp.message_handler(state=Admins.enter_text, content_types=types.ContentTypes.PHOTO)
+async def text_check(message: types.Message, state: FSMContext):
+    photo_id = message.photo[-1].file_id
+    caption = message.caption
+    await state.update_data(photo=photo_id)
+    await state.update_data(caption=caption)
+    await message.answer(f'Проверьте введенное сообщение(оступы и форматирование сохранено)\n\n',
+                         reply_markup=cancel_or_accept)
+    await message.answer_photo(photo_id, caption=caption)
+    await Admins.check_text.set()
+
+
+@dp.message_handler(state=Admins.enter_text, content_types=types.ContentTypes.DOCUMENT)
+async def text_check(message: types.Message, state: FSMContext):
+    file_id = message.document.file_id
+    caption = message.caption
+    await state.update_data(file=file_id)
+    await state.update_data(caption=caption)
+    await message.answer(f'Проверьте введенное сообщение(оступы и форматирование сохранено)\n\n',
+                         reply_markup=cancel_or_accept)
+    await message.answer_document(file_id, caption=caption)
+    await Admins.check_text.set()
+
+
+@dp.message_handler(state=Admins.enter_text, content_types=types.ContentTypes.TEXT)
 async def text_check(message: types.Message, state: FSMContext):
     text = message.html_text
     await state.update_data(post=text)
-    await message.answer(f'Проверьте введеный текст(оступы и форматирование сохранено)\n\n'
+    await message.answer(f'Проверьте введенное сообщение(оступы и форматирование сохранено)\n\n'
                          f'{text}', reply_markup=cancel_or_accept)
     await Admins.check_text.set()
 
 
 @dp.message_handler(state=Admins.check_text, text='Одобрить')
 async def choose_time(message: types.Message, state: FSMContext):
-    await message.answer('Пост начал отправляться юзерам\n'
-                         '(бот будет нагружен в течении небольшого времени(2-5 минут)',
+    await message.answer('Начал отправку',
                          reply_markup=keyboard_for_admin)
     data = await state.get_data()
     post = data.get('post')
-    await broadcaster(post=post)
+    photo = data.get('photo')
+    caption = data.get('caption')
+    video = data.get('video')
+    file = data.get('file')
+    count = await broadcaster(post=post, photo=photo, video=video, caption=caption, file=file)
+    await message.answer(f'<b>{count}</b> messages successful sent.')
     await state.finish()
 
 
-@dp.message_handler(state=Admins.check_text, text='⬅️ Отмена')
+@dp.message_handler(IsAdmin(), state='*', text='⬅️ Отмена')
 async def choose_time(message: types.Message, state: FSMContext):
     await message.answer('Отмена отправки поста', reply_markup=keyboard_for_admin)
     await state.finish()
--- utils/mailing.py
+++ utils/mailing.py
@@ -33,9 +33,9 @@ async def send_message(user_id: int, text: str, disable_notification: bool = Fal
     return False
 
 
-async def send_photo(user_id: int, photo, disable_notification: bool = False) -> bool:
+async def send_photo(user_id: int, photo, caption=None, disable_notification: bool = False) -> bool:
     try:
-        await dp.bot.send_photo(user_id, photo, disable_notification=disable_notification)
+        await dp.bot.send_photo(user_id, photo, caption=caption, disable_notification=disable_notification)
     except exceptions.BotBlocked:
         log.error(f"Target [ID:{user_id}]: blocked by user")
     except exceptions.ChatNotFound:
@@ -54,7 +54,49 @@ async def send_photo(user_id: int, photo, disable_notification: bool = False) ->
     return False
 
 
-async def broadcaster(post=None, photo=None, video=None, file=None) -> int:
+async def send_video(user_id: int, video, caption=None, disable_notification: bool = False) -> bool:
+    try:
+        await dp.bot.send_video(user_id, video, caption=caption, disable_notification=disable_notification)
+    except exceptions.BotBlocked:
+        log.error(f"Target [ID:{user_id}]: blocked by user")
+    except exceptions.ChatNotFound:
+        log.error(f"Target [ID:{user_id}]: invalid user ID")
+    except exceptions.RetryAfter as e:
+        log.error(f"Target [ID:{user_id}]: Flood limit is exceeded. Sleep {e.timeout} seconds.")
+        await asyncio.sleep(e.timeout)
+        return await send_photo(user_id, video)  # Recursive call
+    except exceptions.UserDeactivated:
+        log.error(f"Target [ID:{user_id}]: user is deactivated")
+    except exceptions.TelegramAPIError:
+        log.exception(f"Target [ID:{user_id}]: failed")
+    else:
+        log.info(f"Target [ID:{user_id}]: success")
+        return True
+    return False
+
+
+async def send_file(user_id: int, file, caption=None, disable_notification: bool = False) -> bool:
+    try:
+        await dp.bot.send_document(user_id, file, caption=caption, disable_notification=disable_notification)
+    except exceptions.BotBlocked:
+        log.error(f"Target [ID:{user_id}]: blocked by user")
+    except exceptions.ChatNotFound:
+        log.error(f"Target [ID:{user_id}]: invalid user ID")
+    except exceptions.RetryAfter as e:
+        log.error(f"Target [ID:{user_id}]: Flood limit is exceeded. Sleep {e.timeout} seconds.")
+        await asyncio.sleep(e.timeout)
+        return await send_photo(user_id, file)  # Recursive call
+    except exceptions.UserDeactivated:
+        log.error(f"Target [ID:{user_id}]: user is deactivated")
+    except exceptions.TelegramAPIError:
+        log.exception(f"Target [ID:{user_id}]: failed")
+    else:
+        log.info(f"Target [ID:{user_id}]: success")
+        return True
+    return False
+
+
+async def broadcaster(post=None, photo=None, video=None, file=None, caption=None) -> int:
     count = 0
     if post is not None:
         for user_id in await get_users():
@@ -63,7 +105,17 @@ async def broadcaster(post=None, photo=None, video=None, file=None) -> int:
             await asyncio.sleep(.05)  # 20 messages per second (Limit: 30 messages per second)
     if photo is not None:
         for user_id in await get_users():
-            if await send_photo(user_id, photo=photo):
+            if await send_photo(user_id, photo=photo, caption=caption):
+                count += 1
+            await asyncio.sleep(.05)  # 20 messages per second (Limit: 30 messages per second)
+    if video is not None:
+        for user_id in await get_users():
+            if await send_video(user_id, video=video, caption=caption):
+                count += 1
+            await asyncio.sleep(.05)  # 20 messages per second (Limit: 30 messages per second)
+    if file is not None:
+        for user_id in await get_users():
+            if await send_file(user_id, file=file, caption=caption):
                 count += 1
             await asyncio.sleep(.05)  # 20 messages per second (Limit: 30 messages per second)
     log.info(f"{count} messages successful sent.")
