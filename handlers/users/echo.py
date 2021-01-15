79719ddf8cf7c536b87cc25bab63c15c5428ed74
Dkodsy <dkods15@mail.ru>
2021-01-15 23:52:12 +0600

    Возможность получить сообщение по ключевым словам

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -7,15 +7,25 @@ from loader import dp
 # Эхо хендлер, куда летят текстовые сообщения без указанного состояния
 @dp.message_handler(state=None)
 async def bot_echo(message: types.Message):
-    await message.answer(f"Эхо без состояния."
-                         f"Сообщение:\n"
-                         f"{message.text}")
+    if message.text.lower() == 'вече':
+        await message.answer('Ура! Кодовое слово названо верно😉\n'
+                             'Держи свой бонус «Ассоциации нашей и всемирной истории» по ссылке ниже»\n\n'
+                             'easyhist.ru/bonus\n'
+                             'easyhist.ru/bonus\n'
+                             'easyhist.ru/bonus')
+    elif message.text.lower() == 'барщина':
+        await message.answer('Ура! Кодовое слово названо верно😉\n'
+                             'Держи свой бонус «Частые ошибки на ЕГЭ»\n\n'
+                             'easyhist.ru/secret\n'
+                             'easyhist.ru/secret\n'
+                             'easyhist.ru/secret')
+    else:
+        await message.answer('Вероятно, ты хочешь ввести кодовое слово из урока, ты не угадал, попробуй еще раз😊\n'
+                             'Если у тебя есть какой-то вопрос, пиши @help_easyhist')
 
 
 # Эхо хендлер, куда летят ВСЕ сообщения с указанным состоянием
 @dp.message_handler(state="*", content_types=types.ContentTypes.ANY)
-async def bot_echo_all(message: types.Message, state: FSMContext):
-    state = await state.get_state()
-    await message.answer(f"Эхо в состоянии <code>{state}</code>.\n"
-                         f"\nСодержание сообщения:\n"
-                         f"<code>{message}</code>")
+async def bot_echo_all(message: types.Message):
+    await message.answer('Вероятно, ты хочешь ввести кодовое слово из урока, ты не угадал, попробуй еще раз😊\n'
+                         'Если у тебя есть какой-то вопрос, пиши @help_easyhist')
