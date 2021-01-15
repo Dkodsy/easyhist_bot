b13d60f0802db646b41c4128c7d6116829982f02
Dkodsy <dkods15@mail.ru>
2021-01-16 04:02:17 +0600

    Проебался с фоткой

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -8,8 +8,7 @@ from loader import dp, scheduler, db
 video = 'BAACAgIAAxkBAAONYAHPqVJoisfMPTjGazquinzJoQwAAqYKAAJQeRFIcFtC2u4YDbweBA'
 photo_1 = 'AgACAgIAAxkBAAMKYAHDd5N_nSsQ7zzVVnAM4uoMFNcAAi6xMRtQeRFIWMIGHltEi1lMwimbLgADAQADAgADeQADCFsBAAEeBA'
 file_message_four = 'BQACAgIAAxkBAAMOYAHD2RT_PdTpr9kDiAsT0EEDyH8AAnYKAAJQeRFIskgmVHTMtoseBA'
-photo_message_four = 'AgACAgIAAxkBAAMMYAHDtS-_KOa0hbxywjbLsx6-rHUAAi' \
-                     '-xMRtQeRFIPVpNbKLGjVBO6xeYLgADAQADAgADeQADybIFAAEeBA '
+photo_message_four = 'AgACAgIAAxkBAAMMYAHDtS-_KOa0hbxywjbLsx6-rHUAAi-xMRtQeRFIPVpNbKLGjVBO6xeYLgADAQADAgADeQADybIFAAEeBA '
 
 
 @dp.message_handler(CommandStart())
